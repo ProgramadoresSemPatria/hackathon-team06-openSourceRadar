93b40b9448a3b49182bf2fbd888f53c026c1c0ea
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-03-26 02:18:01 -0300

    feat: remap login redirect to onboarding page

--- front-end/src/pages/public/Login.tsx
+++ front-end/src/pages/public/Login.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     try {
       setLoading(true);
       await signIn();
-      navigate("/dashboard");
+      navigate("/onboarding");
     } catch (err) {
       console.error("Erro no login:", err);
       toast.error("Failed to login with GitHub. Please, try again.");
@@ -34,39 +34,26 @@ export default function Login() {
         <form>
           <div className="flex flex-col gap-6">
             <div className="flex flex-col items-center gap-2">
-              <a
-                href="#"
-                className="flex flex-col items-center gap-2 font-medium"
-              >
+              <a href="#" className="flex flex-col items-center gap-2 font-medium">
                 <div className="flex size-8 items-center justify-center rounded-md">
                   <GitPullRequestArrow />
                 </div>
                 <span className="sr-only">Acme Inc.</span>
               </a>
-              <h1 className="text-xl text-center font-bold">
-                Welcome to Open Souce radar
-              </h1>
+              <h1 className="text-xl text-center font-bold">Welcome to Open Souce radar</h1>
               <div className="text-center text-sm text-muted-foreground">
-                Continue with github to unlock tailored content and stay
-                connected with your community.
+                Continue with github to unlock tailored content and stay connected with your community.
               </div>
             </div>
 
-            <Button
-              onClick={handleGitHubLogin}
-              variant="default"
-              type="button"
-              className="w-full"
-              disabled={loading}
-            >
+            <Button onClick={handleGitHubLogin} variant="default" type="button" className="w-full" disabled={loading}>
               <Github />
               Continue with Github
             </Button>
           </div>
         </form>
         <div className="text-muted-foreground *:[span]:hover:text-primary text-center text-xs text-balance *:[span]:underline *:[span]:underline-offset-4">
-          Your information is securely <span>protected</span>, and we never
-          store your credentials
+          Your information is securely <span>protected</span>, and we never store your credentials
         </div>
       </div>
     </div>

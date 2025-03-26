9689cee163763def7c47dbc81ba19ffb2b499481
carloshenryck <ryck302@gmail.com>
2025-03-25 21:54:59 -0300

    fix: uncomment protected layout

--- front-end/src/components/ProtectedLayout.tsx
+++ front-end/src/components/ProtectedLayout.tsx
@@ -4,9 +4,9 @@ import { useAuth } from "../lib/AuthContext";
 export default function ProtectedLayout() {
   const { currentUser } = useAuth();
 
-  /*   if (!currentUser) {
+  if (!currentUser) {
     return <Navigate to="/login" />;
-  } */
+  }
 
   return <Outlet />;
 }

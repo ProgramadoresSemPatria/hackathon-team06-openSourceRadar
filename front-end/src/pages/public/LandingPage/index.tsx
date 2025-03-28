f5f00fd53b0011a908e79d242a40219f1a53cd3e
carloshenryck <ryck302@gmail.com>
2025-03-28 17:03:56 -0300

    style: responsiveness of landing page

--- front-end/src/pages/public/LandingPage/index.tsx
+++ front-end/src/pages/public/LandingPage/index.tsx
@@ -3,7 +3,7 @@ import SearchSection from "@/pages/public/LandingPage/SearchSection";
 
 export function LandingPage() {
   return (
-    <div className="max-w-[96rem] w-full mx-auto [&>section]:px-8 [&>section]:md:px-24 [&>section]:py-20">
+    <div className="max-w-[96rem] w-full mx-auto [&>section]:px-8 [&>section]:lg:px-24 [&>section]:py-20">
       <HeroSection />
       <SearchSection />
     </div>

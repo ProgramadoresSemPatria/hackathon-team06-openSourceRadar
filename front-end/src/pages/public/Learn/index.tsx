2747464952ae3cc6e0976beede72e805939200a5
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-03-29 22:50:45 -0300

    feat: create public Learn page with educational content about open source

--- front-end/src/pages/public/Learn/index.tsx
+++ front-end/src/pages/public/Learn/index.tsx
@@ -1,4 +1,3 @@
-// src/pages/public/Learn/index.tsx
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { GitBranch, Github, Award, Code, BookOpen, Users } from "lucide-react";
 import { Navbar } from "@/components/Navbar";

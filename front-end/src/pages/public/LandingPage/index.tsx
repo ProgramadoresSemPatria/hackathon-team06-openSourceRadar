741b581a115897421a760477d884f1bd58e7761b
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-03-29 22:03:34 -0300

    feat: add navbar to landing page for consistent navigation

--- front-end/src/pages/public/LandingPage/index.tsx
+++ front-end/src/pages/public/LandingPage/index.tsx
@@ -1,11 +1,16 @@
+// src/pages/public/LandingPage/index.tsx
 import { HeroSection } from "./HeroSection";
 import SearchSection from "@/pages/public/LandingPage/SearchSection";
+import { Navbar } from "@/components/Navbar";
 
 export function LandingPage() {
   return (
-    <div className="max-w-[96rem] w-full mx-auto [&>section]:px-8 [&>section]:lg:px-24 [&>section]:py-20">
-      <HeroSection />
-      <SearchSection />
+    <div className="max-w-[96rem] w-full mx-auto">
+      <Navbar />
+      <div className="[&>section]:px-8 [&>section]:lg:px-24 [&>section]:py-20">
+        <HeroSection />
+        <SearchSection />
+      </div>
     </div>
   );
 }
